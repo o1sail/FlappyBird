71cbeca0cda2d52fe1b667b2998634bbe31fe51c
kazuho <about.o1sail@gmail.com>
2020-07-28 13:12:39 +0900

    最初

---
+++ FlappyBird/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlappyBird
+//
+//  Created by MacBook Pro on 2020/07/28.
+//  Copyright © 2020 7o1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
